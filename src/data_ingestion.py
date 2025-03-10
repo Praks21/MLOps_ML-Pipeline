5ce6ecb749efcc600a30dcbb7084ec9d48a60747
Praks21 <101043744+Praks21@users.noreply.github.com>
2025-03-10 20:16:38 +0530

    params configured for the pipeline

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -3,7 +3,8 @@
 import pandas as pd  # as we have to work with data frame
 import os            # used to make directory where we will push our data
 from sklearn.model_selection import train_test_split # prevent data leakage
-import logging  # 
+import logging  
+import yaml
 
 # ensure the "logs" directory exists
 log_dir='logs'
@@ -30,6 +31,23 @@ file_handler.setFormatter(formatter)
 # above to lines or handler , now we will put that output in logger
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
+
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
 
 # creating function to load the data
 def load_data(data_url: str) -> pd.DataFrame:
@@ -76,7 +94,10 @@ def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str)
 # defineing the main function
 def main():
     try:
-        test_size=0.2
+        # test_size=0.2
+        params = load_params(params_path='params.yaml')
+        test_size = params['data_ingestion']['test_size']
+        
         data_path='https://raw.githubusercontent.com/vikashishere/YT-MLOPS-Complete-ML-Pipeline/refs/heads/main/experiments/spam.csv'
         df = load_data(data_url=data_path)
         final_df = preprocess_data(df)
